98cea45c43f59fa84cdad7a1087a50030bc3d3a5
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-06 01:39:00 +0800

    fix width of banner

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -5,7 +5,7 @@ import bgmobile from "../assets/home/background-home-mobile.jpg";
 function Homepage() {
     return(
         <>
-            <div className="">
+            <div className="banner">
                 <img className="desk" src={bgdesktop} alt="bg-desktop" />
                 <img className="tab" src={bgtablet} alt="bg-tablet" />
                 <img className="mob" src={bgmobile} alt="bg-mobile" />
