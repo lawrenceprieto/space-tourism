d39bef22cb9bc4dafbd070882ef777d2156d5218
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-05 12:28:19 +0800

    set up react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import NavBar from "./component/navbar"
+import CrewPage from "./pages/crew"
+import DestinationPage from "./pages/destination"
 import Homepage from "./pages/homepage"
+import NoPage from "./pages/nopage"
+import TechnologyPage from "./pages/technology"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <>
-      <Homepage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<Homepage />} />
+          <Route path="/destination" element={<DestinationPage />} />
+          <Route path="/crew" element={<CrewPage />} />
+          <Route path="/technology" element={<TechnologyPage />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ src/component/navbar.jsx
@@ -0,0 +1,27 @@
+import { Outlet, Link } from "react-router-dom";
+
+function NavBar() {
+    return(
+        <>
+        <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/destination">Destination</Link>
+          </li>
+          <li>
+            <Link to="/crew">Crew</Link>
+          </li>
+          <li>
+            <Link to="/technology">Technology</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+        </>
+    );
+}
+
+export default NavBar;
