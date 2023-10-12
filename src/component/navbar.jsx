f883fde7cc5611757a67c878ac344738fa49f5d9
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-12 19:59:00 +0800

    fixed properties error

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -15,7 +15,7 @@ function NavBar() {
                     </ul>
                 </div>
                 <div className="mob">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-list text-white" viewBox="0 0 16 16"
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-list text-white" viewBox="0 0 16 16"
                         data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
                         <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                     </svg>                    
