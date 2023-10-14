2f509ab3346e12f106992fbbafeb8eac13601843
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-14 20:04:17 +0800

    update technology page

--- src/pages/technology.jsx
+++ src/pages/technology.jsx
@@ -5,20 +5,21 @@ import spaceCapsuleLandscape from "../assets/technology/image-space-capsule-land
 import spaceCapsulePortrait from "../assets/technology/image-space-capsule-portrait.jpg";
 import spaceportLandscape from "../assets/technology/image-spaceport-landscape.jpg";
 import spaceportPortrait from "../assets/technology/image-spaceport-portrait.jpg";
+import { useState } from "react";
 
 function TechnologyPage() {
 
     const techs = [
         {
-            techImgPortrait: launchVehiclePortrait,
-            techImgLandscape: launchVehicleLandscape,
-            techName: "launch vehicle",
-            techAbout: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch",
-        },{
             techImgPortrait: spaceportPortrait,
             techImgLandscape: spaceportLandscape,
             techName: "spaceport",
             techAbout: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+        },{
+            techImgPortrait: launchVehiclePortrait,
+            techImgLandscape: launchVehicleLandscape,
+            techName: "launch vehicle",
+            techAbout: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch",
         },{
             techImgPortrait: spaceCapsulePortrait,
             techImgLandscape: spaceCapsuleLandscape,
@@ -27,11 +28,35 @@ function TechnologyPage() {
         },
     ];
 
+    const [activeIndex, setActiveIndex] = useState(0);
+    const activeTech = techs[activeIndex];
+
+    function handleTechDetails(index) {
+        setActiveIndex(index);
+    }
+
     return (
         <>
             <div className="body-tech">
-                <div className="container-fluid">
+                <div className="container-fluid pb-5">
                     <div className="tech-headline"><span className="text-secondary">03</span> space launch 101</div>
+                    <div className="tech-img-cont">
+                        <img src={activeTech.techImgLandscape} className="tech-image" alt="spaceport-landscape" />
+                    </div>
+                    <div className="tech-navs">
+                        {
+                            techs.map((tech,index) => (
+                                <div className={index === activeIndex ? "tech-nav-active" : "tech-nav"} 
+                                    onClick={() => handleTechDetails(index)} key={index}>{index + 1}
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <div className="tech-dets">
+                        <div className="terminology">the terminology...</div>
+                        <div className="tech-name">{activeTech.techName}</div>
+                        <div className="tech-about">{activeTech.techAbout}</div>
+                    </div>
                 </div>
             </div>
         </>
