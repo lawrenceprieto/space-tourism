05221d7381e010e6e33a492866e6f0b7e1e5727f
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-15 13:46:53 +0800

    refactor desktop size

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -19,14 +19,14 @@ function NavBar() {
     return(
         <>
             <div className="header">
-                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"><g fill="none" fillRule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" className="dest-logo" width="50" height="50"><g fill="none" fillRule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg>
                 <div className="navbar tab-desk">
                     <ul>
                         {
                             navs.map((nav,index) => (
                                 <li key={index}>
                                     <Link to={nav.linkTo} className={index === activeIndex ? "active-navbar" : null} 
-                                        onClick={() => handleNav(index)} ><span>{index}</span> {nav.name}
+                                        onClick={() => handleNav(index)} ><span className="nav-num">0{index}</span> {nav.name}
                                     </Link>
                                 </li>
                             ))
