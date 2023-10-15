24ed3491b71d27c891115d3711cda7677d597976
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-15 21:16:57 +0800

    fixed alignment of homepage

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -3,21 +3,23 @@ import "../styles/homepage.css";
 function Homepage() {
     return(
         <>
-        <div className="body-hp">
-        <div className="container-fluid banner-heading">
-                <div className="">
-                    <h3>so, you want to travel to</h3>
-                    <h1>space</h1>
-                    <p>
-                        lets face it; if you wan to go space, you might as well genuinely 
-                        go to outer space and not hover kind of on the edge of it. Well 
-                        sit back, and relax beacuse we'll give you a truly out of this 
-                        world experience!
-                    </p>
+            <div className="body-hp">
+                <div className="container-fluid" style={{paddingBottom: "250px"}}>
+                    <div className="home-dest">
+                        <div className="home-det">
+                            <div className="home-heading1">so, you want to travel to</div>
+                            <div className="home-heading2">space</div>
+                            <div className="home-subhead">
+                                lets face it; if you wan to go space, you might as well genuinely 
+                                go to outer space and not hover kind of on the edge of it. Well 
+                                sit back, and relax beacuse we'll give you a truly out of this 
+                                world experience!
+                            </div>
+                        </div>
+                        <div className="explore">explore</div>
+                    </div>
                 </div>
-                <div className="explore">explore</div>
             </div>
-        </div>
         </>
     );
 }
