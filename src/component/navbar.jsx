afef46c12b6b4b8aedae982d673776e56fbbe401
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-15 12:40:03 +0800

    refactor navigation add an active nav

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,17 +1,36 @@
 import { Outlet, Link } from "react-router-dom";
 import "../styles/navbar.css";
+import { useState } from "react";
 
 function NavBar() {
+
+    const [activeIndex, setActiveIndex] = useState(0);
+    const navs = [
+        { name: "Home", linkTo: "/" },
+        { name: "Destination", linkTo: "/destination" },
+        { name: "Crew", linkTo: "/crew" },
+        { name: "Technology", linkTo: "/technology" },
+    ]; 
+
+    function handleNav(index) {
+        setActiveIndex(index);
+    }
+
     return(
         <>
             <div className="header">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"><g fill="none" fillRule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg>
                 <div className="navbar tab-desk">
                     <ul>
-                        <li><Link to="/"><span>00</span> Home</Link></li>
-                        <li><Link to="/destination"><span>01</span> Destination</Link></li>
-                        <li><Link to="/crew"><span>02</span> Crew</Link></li>
-                        <li><Link to="/technology"><span>03</span> Technology</Link></li>
+                        {
+                            navs.map((nav,index) => (
+                                <li key={index}>
+                                    <Link to={nav.linkTo} className={index === activeIndex ? "active-navbar" : null} 
+                                        onClick={() => handleNav(index)} ><span>{index}</span> {nav.name}
+                                    </Link>
+                                </li>
+                            ))
+                        }
                     </ul>
                 </div>
                 <div className="mob">
