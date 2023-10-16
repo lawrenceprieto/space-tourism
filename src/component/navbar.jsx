95e0f7e4800dfa3db206999b86be4cdde1aad293
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-16 12:46:58 +0800

    changed color and size of menu button

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -34,11 +34,10 @@ function NavBar() {
                     </ul>
                 </div>
                 <div className="mob">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-list text-white" viewBox="0 0 16 16"
-                        data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="offcanvasExample">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16"
+                        data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="offcanvasExample" style={{color: "#D0D6F9"}}>
                         <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                    </svg>                    
-                    
+                    </svg>
                     <div style={{width: "300px"}}className="offcanvas offcanvas-end mob" tabIndex="-1" id="offcanvas" aria-labelledby="offcanvasExampleLabel">
                     <div className="offcanvas-header">
                         <button type="button" className="close-btn" data-bs-dismiss="offcanvas" aria-label="Close">
