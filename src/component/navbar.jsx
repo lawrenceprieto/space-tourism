cdc7df438f8ddea072ac6fe7704b9248e9a0144a
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-16 12:36:22 +0800

    change the menu close button

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -41,7 +41,9 @@ function NavBar() {
                     
                     <div style={{width: "300px"}}className="offcanvas offcanvas-end mob" tabIndex="-1" id="offcanvas" aria-labelledby="offcanvasExampleLabel">
                     <div className="offcanvas-header">
-                        <button type="button" className="close-btn" data-bs-dismiss="offcanvas" aria-label="Close">x</button>
+                        <button type="button" className="close-btn" data-bs-dismiss="offcanvas" aria-label="Close">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21"><g fill="#D0D6F9" fillRule="evenodd"><path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z"/><path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z"/></g></svg>
+                        </button>
                     </div>
                     <div className="offcanvas-body">
                         <div className="navbar">
